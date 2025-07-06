a962fa858e49138087afb49beea9e709dbfc4a3f
Shinyvv <shinyadaniel0@gmail.com>
2025-07-06 00:27:12 -0400

    Tratando de arreglar canon url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://xn--cabaasplayaguardiamo-66b.cl"
+  const baseUrl = "https://cabañasplayaguardiamo.cl"
   const routes = ["/", "/cabanas", "/contacto", "/galeria", "/programa", "/ubicacion"]
 
   return routes.map((route) => ({
